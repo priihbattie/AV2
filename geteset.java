0ce633759192c4ecc43d589f66d462efe1b65085
priihbattie <135707297+priihbattie@users.noreply.github.com>
2024-10-17 08:19:06 -0300

    Update geteset.java

--- geteset.java
+++ geteset.java
@@ -3,5 +3,22 @@ public class TestaGetESet {
         Conta conta = new Conta();
         conta.setNumero(1337);
         System.out.println(conta.getNumero()); 
+        
+        Cliente micaela = new Cliente(); 
+    micaela.setNome("micaela herrera");
+
+    conta.setTitular(micaela);
+
+    System.out.println(conta.getTitular().getNome());
+
+        conta.getTitular().setProfissao("secretaria"); 
+        //agora em duas linhas: 
+        Cliente titularDaConta = conta.getTitular();
+        titularDaConta.setProfissao("secretaria");
+
+        System.out.println(titularDaConta); 
+        System.out.println(micaela);
+        System.out.println(conta.getTitular());
+
     }
 }
