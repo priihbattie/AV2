eb5932eb4161182e1447b28b24033c976af6bfdb
priihbattie <135707297+priihbattie@users.noreply.github.com>
2024-10-17 08:37:45 -0300

    Add files via upload

---
+++ testefuncionario.java
@@ -0,0 +1,14 @@
+public class TesteFuncionario {
+
+    public static void main(String[] args) {
+
+        Funcionario nico = new Funcionario();
+        micaela.setNome("Micaela Herrera");
+        micaela.setCpf("111.222.333-44");
+        micaela.setSalario(2600.00);
+
+        System.out.println(micaela.getNome());
+        System.out.println(micaela.getBonificacao());
+
+    }
+}
